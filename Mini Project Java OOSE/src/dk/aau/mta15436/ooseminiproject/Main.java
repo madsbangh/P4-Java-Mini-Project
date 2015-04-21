c5677a2884808d4a5b5a40292cb9298d79aeac6e
Mads Bang Hoffensetz <faetter.mads@hotmail.com>
2015-04-21 10:49:00 +0200

    Created folder structure. Created Main class with boiler-plate code. Added processing library.

---
+++ Mini Project Java OOSE/src/dk/aau/mta15436/ooseminiproject/Main.java
@@ -0,0 +1,14 @@
+package dk.aau.mta15436.ooseminiproject;
+
+import processing.core.*;
+
+public class Main extends PApplet
+{
+	private static final long serialVersionUID = 3286085431838033816L;
+
+	public static void main(String[] args)
+	{
+		PApplet.main(new String[] { "dk.aau.mta15436.ooseminiproject.Main" });
+	}
+
+}
